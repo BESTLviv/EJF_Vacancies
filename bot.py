a7a92992c9f4f300dcdfa1fd93c83ff1a0ca7c34
HYaroslav <gslaavko@gmail.com>
2021-03-24 23:40:48 +0200

    small updates

--- bot.py
+++ bot.py
@@ -1,10 +1,26 @@
 import configparser
-from telebot import Telebot
+from data import Data
+from telebot import TeleBot, logger
 
 config = configparser.ConfigParser()
 config.read('Settings.ini')
 
 API_TOKEN = config['TG']['token']
+DB = config['Mongo']['db']
+
+bot = TeleBot(API_TOKEN, parse_mode="HTML")
+data = Data(conn_string=DB)
+
+@bot.message_handler(commands=['start'])
+def start_bot(message):
+    chat_id = message.chat.id
+    username = message.chat.username if message.chat.username is not None else "Безіменний"
+    name = message.chat.first_name if message.chat.first_name is not None else "Безіменний"
+    surname = message.chat.last_name if message.chat.last_name is not None else "0"
+    
+    data.add_user(chat_id=chat_id, name=name, surname=surname, username=username)
+
+    
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
--- data.py
+++ data.py
@@ -1,7 +1,28 @@
 import telebot
 import mongoengine as me
+from datetime import datetime
 
-me.connect()
+#me.connect()
+
+class Data:
+
+    def __init__(self, conn_string):
+        me.connect(host=conn_string)
+        print("connection success ")
+
+    def add_user(self, chat_id, name, surname, username, interests=[], experience="", 
+                 employment="", apply_counter=0, registration_date=None, last_update_date=None, 
+                 last_interaction_date=None, hr_status=False):
+
+        registration_date = datetime.now()
+        last_interaction_date = registration_date
+        last_update_date = registration_date
+
+        User(chat_id=chat_id, name=name, surname=surname, username=username,
+             interests=interests, experience=experience, employment=employment,
+             apply_counter=apply_counter, registration_date=registration_date,
+             last_update_date=last_update_date, last_interaction_date=last_interaction_date,
+             hr_status=hr_status).save()
 
 
 class User(me.Document):
@@ -9,7 +30,7 @@ class User(me.Document):
     name = me.StringField(required=True)
     surname = me.StringField(required=True)
     username = me.StringField(required=True)
-    interests = me.ListField(required=True) #
+    interests = me.ListField(required=False) #
     experience = me.StringField(required=True) #
     employment = me.StringField(required=True) #
     # CV = me.StringField(required=True)
@@ -17,8 +38,7 @@ class User(me.Document):
     registration_date = me.DateField(required=True) #
     last_update_date = me.DateField(required=True)
     last_interaction_date = me.DateField(required=True)
-    hr_status = me.StringField(required=True)
-
+    hr_status = me.BooleanField(required=True)
 
 class Vacancy(me.EmbeddedDocument):
     name = me.StringField(required=True)
@@ -27,10 +47,9 @@ class Vacancy(me.EmbeddedDocument):
     employment_type = me.StringField(required=True)
     description = me.StringField(required=True)
     post_date = me.StringField(required=True)
-    last_update_date = me.StringField(required=True)
+    last_update_date = me.DateField(required=True)
     active_days_left = me.StringField(required=True)
     status = me.StringField(required=True)
-
 
 class Company(me.Document):
     name = me.StringField(required=True)
@@ -42,43 +61,40 @@ class Company(me.Document):
     registration_date = me.DateField(required=True)
     vacancy_list = me.ListField(me.EmbeddedDocumentField, Vacancy, required=True)
 
-
 class VacancyPreviewLog(me.Document):
     """
     Class for logging user viewing of vacancy.
     """
-    vacancy_id = me.StringField(required=True)
-    time = me.StringField(required=True)
+    vacancy_id = me.ReferenceField(Vacancy, required=True)
     user_id = me.StringField(required=True)
-
+    log_datetime = me.DateField(required=True)
 
 class VacancyApplyLog(me.Document):
     """
     Class for logging any applying for a job.
     """
-    vacancy_id = me.StringField(required=True)
+    vacancy_id = me.ReferenceField(Vacancy, required=True)
     user_id = me.StringField(required=True)
     cv = me.StringField(required=True)
-    time = me.StringField(required=True)
-
+    log_datetime = me.DateField(required=True)
 
 class VacancyPromotionLog(me.Document):
     """
-    Class for logging every logging.
+    Class for logging every promotion.
     """
-    time = me.StringField(required=True)
-    vacancy_id = me.StringField(required=True)
+    vacancy_id = me.ReferenceField(Vacancy, required=True)
     auditory_tag = me.StringField(required=True)
+    log_datetime = me.DateField(required=True)
 
 
 if __name__ == '__main__':
-    table1 = User().save()  # insert new user in table
+    #table1 = User().save()  # insert new user in table
 
     vac1 = Vacancy()
     vac2 = Vacancy()
     vacancies = [vac1, vac2]
     table2 = Company(name='Salam', photo_id='1', description='fff', vacancy_counter='5', HR_chat_id='1',
                      token='urhfhsf1', registration_date='2012-02-02', vacancy_list=vacancies).save()
-    table4 = VacancyPreviewLog().save()
-    table5 = VacancyApplyLog().save()
-    table6 = VacancyPromotionLog().save()
+    #table4 = VacancyPreviewLog().save()
+    #table5 = VacancyApplyLog().save()
+    #table6 = VacancyPromotionLog().save()
