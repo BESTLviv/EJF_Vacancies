b1b8d8cce00de4bb75dc0cca3069c6bf55ab0b2c
HYaroslav <gslaavko@gmail.com>
2021-04-05 21:29:55 +0300

    interests update

--- src/objects/interests.py
+++ src/objects/interests.py
@@ -1,5 +1,10 @@
+from ..data import (
+    User
+)
 
 
+def process_callback(call, user: User):
+    pass
 
 def create_interests_markup():
     pass
@@ -11,3 +16,7 @@ def create_experience_markup():
 
 def create_work_time_markup():
     pass
+
+
+def form_user_interests_callback(self, action, is_on, interest_index="", experience_index="", employment_index="", user_id="", prev_msg_action=None):
+    return f"User;{action};{is_on};{interest_index};{experience_index};{employment_index};{user_id};{prev_msg_action}"
