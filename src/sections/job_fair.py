fbad88d754e17a46bd3a798df1446539abbf69c9
HYaroslav <gslaavko@gmail.com>
2021-04-09 21:52:16 +0300

    basic structure of task

--- src/sections/job_fair.py
+++ src/sections/job_fair.py
@@ -5,7 +5,12 @@ from ..sections.section import Section
 
 class JobFairSection(Section):
 
-    TEXT_BUTTONS = ["", ""]
+    TEXT_BUTTONS = ["Компанії", "Папап"]
+
+    BUTTONS_DICT = [
+        {"name": "Компанії", "text": "афафіафіа", "photo": "wrgewgewf"},
+        {"name": "Папап", "text": "афафіафіа", "photo": "wrgewgewf"},
+    ]
 
     def __init__(self, data: Data):
         super().__init__(data)
@@ -14,4 +19,19 @@ class JobFairSection(Section):
         pass
 
     def send_start_menu(self, user: User):
-        pass
+
+        markup
+        for btn in self.BUTTONS_DICT:
+            button(text=btn["name"])
+            markup.add(button)
+
+        self.bot.send_message(
+            user.chat_id,
+            text="Кінець.\nТут будуть кнопки ярмарку\n\n/start щоб пройти повторно",
+        )
+
+    def send_button_content(self, user: User, btn_text: str):
+        for btn in self.BUTTONS_DICT:
+            if btn["name"] == btn_text:
+                self.bot.send_photo()
+                return
