b2fde65fc87a813b74a6a3bbf3524981c46c3f54
mershalova <anzhela.mershalova.knm.2020@lpnu.ua>
2021-04-18 18:46:55 +0300

    main menu

--- src/sections/hr.py
+++ src/sections/hr.py
@@ -8,6 +8,7 @@ from ..data import (
 from .section import Section
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
 class HR(Section):
 
     def __init__(self, data: Data):
@@ -17,38 +18,42 @@ class HR(Section):
         action = call.data.split(";")[1]
 
         if action == "VacList":
-            self.send_vacany_list(user)
+            self.send_vacancy_list(user)
 
-        elif action == "":
-            pass
+        elif action == "My_Company":
+            self.show_my_company()
         
-        else:
-            pass
+        elif action == "Sign_out_from_company":
+            self.quit_company_status()
 
     def process_text(self, text):
         pass
 
-    
     def send_start_menu(self, user: User):
         company = Company.objects.filter()[0]
-        
+
         # my vacancies
-        btn_text = "Мої"
-        btn_callback = self.form_hr_callback(action="VacList")
-        btn_my_vacancies = InlineKeyboardButton(btn_text, btn_callback)
+        btn_text1 = "Мої вакансії"
+        btn_text2 = "Моя компанія"
+        btn_text3 = "Вийти з профілю компанії"
 
+        btn_callback_vaclist = self.form_hr_callback(action="VacList")
+        btn_callback_company = self.form_hr_callback(action="My_Company")
+        btn_callback_sign_out = self.form_hr_callback(action="Sign_out_from_company")
 
-        self.bot.send_photo()
+        btn_my_vacancies = InlineKeyboardButton(btn_text1, btn_callback_vaclist)
+        btn_my_company = InlineKeyboardButton(btn_text2, btn_callback_company)
+        btn_sign_out_from_company = InlineKeyboardButton(btn_text3, btn_callback_sign_out)
 
+        markup_inline = InlineKeyboardMarkup()
+        self.bot.send_photo(user, company.photo_id, company.description)
 
-    
+        markup_inline.add(btn_my_vacancies)
+        markup_inline.add(btn_my_company)
+        markup_inline.add(btn_sign_out_from_company)
 
-    def send_vacany_list(self, user: User):
+    def send_vacancy_list(self, user: User):
         pass
-
-
-        
-        
 
     def add_vacancy(self):
         pass
@@ -71,8 +76,8 @@ class HR(Section):
     def change_vacancy_status(self, current_status: int):
         pass
 
-
-
+    def show_my_company(self):
+        pass
 
     def quit_company_status(self, chat_id):
         pass
