1443c3b2f3a01a53269cb9694fa795000af49658
HYaroslav <gslaavko@gmail.com>
2021-04-20 15:43:44 +0300

    added test 'help' menu

--- test_bot.py
+++ test_bot.py
@@ -4,15 +4,27 @@ from telebot import TeleBot
 from src.data import User, JobFair, Data
 
 
-def process_tests_text(bot: TeleBot, user: User, data: Data, text: str):
+help_msg = (
+    "TEST COMMANDS HELP\n\n"
+    "<b>ejf__help</b> - for help message\n"
+    "<b>ejf__fullstart</b> - full start of bot (with start message and quiz)"
+)
+
+
+def process_tests_text(bot_: TeleBot, user: User, data: Data, text: str):
     test_action = text.split("__")[1]
 
     if test_action == "help":
-        pass
+        bot_.send_message(user.chat_id, help_msg)
 
     elif test_action.startswith("edit"):
         btn_number = int(text.split("-")[-1])
 
-    if test_action == "update":
+    elif test_action == "update":
         data.update_ejf_table()
-        bot.send_message(user.chat_id, text="EJF table has been updated")
+        bot_.send_message(user.chat_id, text="EJF table has been updated")
+
+    elif test_action == "fullstart":
+        import bot as bot_file
+
+        bot_file.send_welcome_message_and_start_quiz(user=user)
