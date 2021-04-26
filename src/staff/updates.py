f0d3a374662a11b6407ca414ae764a84b69fd456
HYaroslav <gslaavko@gmail.com>
2021-04-26 18:18:25 +0300

    Added deep linking registration

--- src/staff/updates.py
+++ src/staff/updates.py
@@ -36,12 +36,16 @@ class Updater:
                 if message.chat.last_name is not None
                 else "No Surname"
             )
+            register_source = (
+                message.text.split()[1] if len(message.text.split()) > 1 else "Unknown"
+            )
 
             user = User(
                 chat_id=user_chat_id,
                 name=name,
                 surname=surname,
                 username=username,
+                register_source=register_source,
                 registration_date=date,
                 last_update_date=date,
                 last_interaction_date=date,
